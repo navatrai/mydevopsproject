cabe0e3b45dbc8f2fc1e82239dff6d0c45305ca1
Ubuntu <ubuntu@ip-172-31-35-115.ap-south-1.compute.internal>
2026-04-08 19:18:19 +0000

    update message text

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (request, response) => {
-  response.end('Hello DevOps from Node.js!');
+  response.end('Hello DevOps from Node.js successfully launched!');
 }
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
+}) ;
